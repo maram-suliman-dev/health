057af1268a6f3bf033f18063326b26419364cb28
Maram suliman <mms.bur@outlook.com>
2025-05-07 14:47:03 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace program_Health
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int playerHealth = 85;
+
+                Console.WriteLine("Health before potion: " + playerHealth);
+
+                UseHealthPotion(ref playerHealth);
+
+                Console.WriteLine("Health after potion: " + playerHealth);
+            }
+
+            static void UseHealthPotion(ref int health, int potion = 20)
+            {
+                health += potion;
+
+                if (health > 100)
+                {
+                    health = 100;
+                }
+            }
+        }
+    }
+}
